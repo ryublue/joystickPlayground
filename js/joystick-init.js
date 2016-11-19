27850ed2b571fd68af7b716b0697f6a3ab777521
Chat Wacharamanotham <chat@ifi.uzh.ch>
2016-11-20 00:15:02 +0100

    Remove unused code in joystick module

--- js/joystick-init.js
+++ js/joystick-init.js
@@ -1,64 +1,53 @@
 ( function ($) {
-// create joystick
-(function createJoystick () {
-    joystick = nipplejs.create({
-        zone: $('.zone')[0],
-        color: 'blue',
-        mode: 'static',
-        position: {left: '70%', top: '50%'},
-        multitouch: true
-    });
-    bindJoystick();
-})();
+    // create joystick
+    (function createJoystick () {
+        joystick = nipplejs.create({
+            zone: $('.zone')[0],
+            color: 'blue',
+            mode: 'static',
+            position: {left: '70%', top: '50%'},
+            multitouch: true
+        });
+        bindJoystick();
+    })();
 
-function bindJoystick () {
-    joystick.on('start', function (evt, data) {
-        debug(data);
-    }).on('move', function (evt, data) {
-        debug(data);
-        joystickData = data;
-    }).on('end', function (evt, data) {
-        debug(data);
-        joystickData = null;
-    });
-}
-
-// Get debug elements and map them
-var elDebug = $('#debug')[0];
-var els = {
-    position: {
-        x: elDebug.querySelector('.position .x .data'),
-        y: elDebug.querySelector('.position .y .data')
-    },
-    force: elDebug.querySelector('.force .data'),
-    pressure: elDebug.querySelector('.pressure .data'),
-    distance: elDebug.querySelector('.distance .data'),
-    angle: {
-        radian: elDebug.querySelector('.angle .radian .data'),
-        degree: elDebug.querySelector('.angle .degree .data')
-    },
-    direction: {
-        x: elDebug.querySelector('.direction .x .data'),
-        y: elDebug.querySelector('.direction .y .data'),
-        angle: elDebug.querySelector('.direction .angle .data')
+    function bindJoystick () {
+        joystick.on('start', function (evt, data) {
+            debug(data);
+        }).on('move', function (evt, data) {
+            debug(data);
+            joystickData = data;
+        }).on('end', function (evt, data) {
+            debug(data);
+            joystickData = null;
+        });
     }
-};
+
+    // Get debug elements and map them
+    var elDebug = $('#debug')[0];
+    var els = {
+        distance: elDebug.querySelector('.distance .data'),
+        angle: {
+            radian: elDebug.querySelector('.angle .radian .data'),
+            degree: elDebug.querySelector('.angle .degree .data')
+        }
+    };
 
 
-// Print data into elements
-function debug (obj) {
+    // Print data into elements
+    function debug (obj) {
 
-    function parseObj(sub, el) {
-        for (var i in sub) {
-            if (typeof sub[i] === 'object' && el) {
-                parseObj(sub[i], el[i]);
-            } else if (el && el[i]) {
-                el[i].innerHTML = sub[i].toFixed(0);
+        function parseObj(sub, el) {
+            for (var i in sub) {
+                if (typeof sub[i] === 'object' && el) {
+                    parseObj(sub[i], el[i]);
+                } else if (el && el[i]) {
+                    el[i].innerHTML = sub[i].toFixed(0);
+                }
             }
         }
+        setTimeout(function () {
+            parseObj(obj, els);
+        }, 0);
     }
-    setTimeout(function () {
-        parseObj(obj, els);
-    }, 0);
-}
 } )(jQuery);
