51f808e9fa5b3b9bdda013403a87255d7329a6d4
Chat Wacharamanotham <chat@ifi.uzh.ch>
2016-11-20 00:14:16 +0100

    Merge UI and code editor init

--- js/ui-init.js
+++ js/ui-init.js
@@ -1,6 +1,19 @@
 // initialize UI
 (function ($) {
 
+    // code editor
+    var editor = ace.edit("transferFunctionEditor");
+    editor.setTheme("ace/theme/xcode");
+    var session = editor.getSession()
+    session.setMode("ace/mode/javascript");
+    session.setUseWrapMode(true);
+    session.setWrapLimitRange(40, 45);
+    editor.$blockScrolling = Infinity
+
+    // remember the editor in our namespace
+    config.tfEditor = editor;
+
+
     var updateTransferFunction = function () {
         var funcBody = config.tfEditor.getValue();
         var func = Function("d", funcBody);
