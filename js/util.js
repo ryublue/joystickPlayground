b9ebb62a905b3ff377aa9556ec1866d9da0a5117
Chat Wacharamanotham <chat@ifi.uzh.ch>
2016-11-19 23:06:39 +0100

    All transfer functions work as expected

--- js/util.js
+++ js/util.js
@@ -1,5 +1,7 @@
 var util = ( function ($) {
-    function pointDist (p1, p2) { return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y))); }
+    function pointDist (p1, p2) { 
+        return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y))); 
+    }
 
     var api = {};
     api.pointDist = pointDist;
